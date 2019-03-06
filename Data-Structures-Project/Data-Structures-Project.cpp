718254483644b138cce4128ffffa59459e233096
Yitzak Hernandez <kre3ed@gmail.com>
2019-03-06 17:44:10 -0500

    initial commit

---
+++ Data-Structures-Project/Data-Structures-Project.cpp
@@ -0,0 +1,12 @@
+﻿// Data-Structures-Project.cpp : Defines the entry point for the application.
+//
+
+#include "Data-Structures-Project.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
